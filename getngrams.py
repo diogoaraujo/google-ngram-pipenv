19443eeee763b0b27cd2860849a4df505cb51596
diogoaraujo <diogorjbr@gmail.com>
2022-06-09 22:28:26 -0300

    add pipenv

--- getngrams.py
+++ getngrams.py
@@ -7,38 +7,55 @@ import requests               # http://github.com/kennethreitz/requests
 import subprocess
 import sys
 
-corpora = dict(eng_us_2012=17, eng_us_2009=5, eng_gb_2012=18, eng_gb_2009=6,
-               chi_sim_2012=23, chi_sim_2009=11, eng_2012=15, eng_2009=0,
-               eng_fiction_2012=16, eng_fiction_2009=4, eng_1m_2009=1,
-               fre_2012=19, fre_2009=7, ger_2012=20, ger_2009=8, heb_2012=24,
-               heb_2009=9, spa_2012=21, spa_2009=10, rus_2012=25, rus_2009=12,
-               ita_2012=22)
+corpora = dict(eng_us_2012=17, eng_us_2009=5, eng_us_2019=28,
+            eng_gb_2012=18, eng_gb_2009=6, eng_gb_2019=26,
+            chi_sim_2019=34, chi_sim_2012=23, chi_sim_2009=11,
+            eng_2012=15, eng_2009=0,
+            eng_fiction_2012=16, eng_fiction_2009=4, eng_1m_2009=1,
+            fre_2019=30, fre_2012=19, fre_2009=7,
+            ger_2019=31, ger_2012=20, ger_2009=8,
+            heb_2012=24,
+            heb_2009=9,
+            spa_2019=32, spa_2012=21, spa_2009=10,
+            rus_2019=36, rus_2012=25, rus_2009=12,
+            ita_2019=33, ita_2012=22)
 
 
 def getNgrams(query, corpus, startYear, endYear, smoothing, caseInsensitive):
     params = dict(content=query, year_start=startYear, year_end=endYear,
-                  corpus=corpora[corpus], smoothing=smoothing,
-                  case_insensitive=caseInsensitive)
+                corpus=corpora[corpus], smoothing=smoothing,
+                case_insensitive=caseInsensitive)
     if params['case_insensitive'] is False:
         params.pop('case_insensitive')
     if '?' in params['content']:
         params['content'] = params['content'].replace('?', '*')
     if '@' in params['content']:
         params['content'] = params['content'].replace('@', '=>')
+
     req = requests.get('http://books.google.com/ngrams/graph', params=params)
-    res = re.findall('var data = (.*?);\\n', req.text)
+    res = re.findall('ngrams.data = .*\];', req.text)
+    assert(len(res)==1)
+
     if res:
+        dataDict = literal_eval(res[0].replace(
+            "ngrams.data = ", "").replace(";", ""))
         data = {qry['ngram']: qry['timeseries']
-                for qry in literal_eval(res[0])}
+                for qry in dataDict}
         df = DataFrame(data)
         df.insert(0, 'year', list(range(startYear, endYear + 1)))
     else:
         df = DataFrame()
     return req.url, params['content'], df
 
+def trimSpaceNearComma(argumentString):
+    while (argumentString.find(', ')>=0):
+        argumentString = argumentString.replace(', ',',')
+    while (argumentString.find(' ,')>=0):
+        argumentString = argumentString.replace(' ,',',')
+    return argumentString
 
-def runQuery(argumentString):
-    arguments = argumentString.split()
+def runQuery(argumentString):    
+    arguments = trimSpaceNearComma(argumentString).split()
     query = ' '.join([arg for arg in arguments if not arg.startswith('-')])
     if '?' in query:
         query = query.replace('?', '*')
@@ -80,18 +97,18 @@ def runQuery(argumentString):
             caseInsensitive = False
             notifyUser = True
             warningMessage = "*NOTE: Wildcard and case-insensitive " + \
-                             "searches can't be combined, so the " + \
-                             "case-insensitive option was ignored."
+                            "searches can't be combined, so the " + \
+                            "case-insensitive option was ignored."
         elif '_INF' in query and caseInsensitive is True:
             caseInsensitive = False
             notifyUser = True
             warningMessage = "*NOTE: Inflected form and case-insensitive " + \
-                             "searches can't be combined, so the " + \
-                             "case-insensitive option was ignored."
+                            "searches can't be combined, so the " + \
+                            "case-insensitive option was ignored."
         else:
             notifyUser = False
         url, urlquery, df = getNgrams(query, corpus, startYear, endYear,
-                                      smoothing, caseInsensitive)
+                                    smoothing, caseInsensitive)
         if not allData:
             if caseInsensitive is True:
                 for col in df.columns:
@@ -119,7 +136,7 @@ def runQuery(argumentString):
             for row in df.iterrows():
                 try:
                     print(('%d,' % int(row[1].values[0]) +
-                           ','.join(['%.12f' % s for s in row[1].values[1:]])))
+                        ','.join(['%.12f' % s for s in row[1].values[1:]])))
                 except:
                     print((','.join([str(s) for s in row[1].values])))
         queries = ''.join(urlquery.replace(',', '_').split())
@@ -130,7 +147,7 @@ def runQuery(argumentString):
         else:
             word_case = 'caseSensitive'
         filename = '%s-%s-%d-%d-%d-%s.csv' % (queries, corpus, startYear,
-                                              endYear, smoothing, word_case)
+                                            endYear, smoothing, word_case)
         if toSave:
             for col in df.columns:
                 if '&gt;' in col:
@@ -143,8 +160,8 @@ def runQuery(argumentString):
             except:
                 if not toSave:
                     print(('Currently, if you want to create a plot you ' +
-                           'must also save the data. Rerun your query, ' +
-                           'removing the -nosave option.'))
+                        'must also save the data. Rerun your query, ' +
+                        'removing the -nosave option.'))
                 else:
                     print(('Plotting Failed: %s' % filename))
         if notifyUser:
@@ -159,3 +176,4 @@ if __name__ == '__main__':
             runQuery(argumentString)
         except:
             print('An error occurred.')
+
